597eee7bcdb3748ff158eef86e5f5dcdc67e62ed
RishvinReddy <rishvinreddy@example.com>
2026-05-09 21:51:48 +0530

    Initial commit: Project setup and Home Page

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// SCROLL TO TOP BUTTON
+const scrollBtn = document.getElementById("scrollTopBtn");
+window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        scrollBtn.style.display = "block";
+    } else {
+        scrollBtn.style.display = "none";
+    }
+};
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
